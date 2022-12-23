0ac66e261d8d4843864b3b17976fbd0eb3d42e00
Ajay Ganesh <ajganesh2002@gmail.com>
2022-12-23 01:43:18 -0500

    Setup navbar component

---
+++ components/layout.tsx
@@ -0,0 +1,9 @@
+import { navLinks } from "../src/data"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+        { children }
+        </>
+    )
+}
---
+++ src/data.ts
@@ -0,0 +1,21 @@
+export const navLinks = [
+    { name: "Home", 
+     path: "/" 
+    },
+    {
+      name: "About Me",
+      path: "/#about",
+    },
+    {
+        name: "Projects",
+        path: "/#projects",
+    },
+    {
+      name: "Blog",
+      path: "/blog",
+    },
+    {
+      name: "Contact Me",
+      path: "#contact",
+    },
+  ];
