0d32219da5e62a8a20b4c00e73543d6b2aa88d90
Ajay Ganesh <ajganesh2002@gmail.com>
2023-02-27 22:53:34 -0500

    added headshot

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -1,15 +1,37 @@
+import Image from "next/image";
 import TypeIt from "typeit-react";
 export default function AboutMe() {
-
   return (
     <div className="about justify-center text-center">
       <div className="pb-4">
-        <h1 className="text-5xl pb-4">Ajay Ganesh</h1>
-        <TypeIt options={{
-          strings: ["University of Michigan CS Student", "Software Engineer", "Fullstack Developer", "Mobile Developer"],
-          breakLines: false,
-          loop: true
-        }}/>
+        <div className="flex flex-col items-center justify-center text-center md:flex-row">
+          <a id="about"></a>
+          <div className="relative my-4 h-40 w-40 overflow-hidden rounded-md md:px-20 md:right-6">
+            <Image
+              src="https://portfolio-images-ajayganesh.s3.us-east-2.amazonaws.com/AJGProPhoto.JPG"
+              alt="Pro Headshot"
+              // width={100}
+              // height={100}
+              fill={true}
+              className="object-cover"
+            />
+          </div>
+          <div className="flex flex-col text-center content-center items-center">
+            <h1 className="pb-4 text-5xl">Ajay Ganesh</h1>
+            <TypeIt
+              options={{
+                strings: [
+                  "University of Michigan CS Student",
+                  "Software Engineer",
+                  "Fullstack Developer",
+                  "Mobile Developer",
+                ],
+                breakLines: false,
+                loop: true,
+              }}
+            />
+          </div>
+        </div>
       </div>
       <p>
         Hello! I&apos;m a junior at the University of Michigan studying Computer
